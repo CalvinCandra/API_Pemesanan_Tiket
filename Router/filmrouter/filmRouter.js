1d223848cc4f45cd98ff8f50066d26e80e61b6c2
CalvinCandra <willycandra850@gmail.com>
2025-07-22 09:18:41 +0800

    Solve Bug and Create API Search By Esa

--- Router/filmrouter/filmRouter.js
+++ Router/filmrouter/filmRouter.js
@@ -3,6 +3,10 @@ import {
   CreateFilm,
   UpdateFilm,
   DeleteFilm,
+  ReadFilm,
+  ReadFilmbyid,
+  SearchFilm,
+  CountbyGenre,
 } from "../../Controller/filmcontroller/filmController.js";
 
 const filmRouter = express();
@@ -10,5 +14,9 @@ const filmRouter = express();
 filmRouter.post("/tambahFilm", CreateFilm);
 filmRouter.put("/updateFilm/:id", UpdateFilm);
 filmRouter.delete("/deleteFilm/:id", DeleteFilm);
+filmRouter.get("/searchFilm", ReadFilm);
+filmRouter.get("/searchFilm/:id", ReadFilmbyid);
+filmRouter.post("/searchFilmNamaGenre", SearchFilm);
+filmRouter.post("/searchFilmGenre", CountbyGenre);
 
 export default filmRouter;
